4aa8e27ddddab04a984090c37726100c7db258cb
Cristian <castagna.cristian16@gmail.com>
2022-11-28 00:10:13 -0300

    Implement Chain of Responsability to handle login requests

---
+++ src/main/java/com/crmservice/crmservice/infrastructure/drivers/controllers/LoginController.java
@@ -0,0 +1,31 @@
+package com.crmservice.crmservice.infrastructure.drivers.controllers;
+
+import com.crmservice.crmservice.infrastructure.drivers.requesthandlers.IRequestHandler;
+import com.crmservice.crmservice.infrastructure.drivers.requests.dtos.RequestDTO;
+import com.crmservice.crmservice.infrastructure.drivers.responses.dtos.LoginResponseDTO;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    private final IRequestHandler<Void, Void> basicAuthenticationHeaderHandler;
+
+    public LoginController(
+            IRequestHandler<Void, Void> basicAuthenticationHeaderHandler,
+            IRequestHandler<Void, Void> ipHeaderHandler,
+            IRequestHandler<Void, LoginResponseDTO> loginRequestHandler
+    ) {
+        this.basicAuthenticationHeaderHandler = basicAuthenticationHeaderHandler;
+        this.basicAuthenticationHeaderHandler
+                .setNext(ipHeaderHandler)
+                .setNext(loginRequestHandler);
+    }
+
+    @GetMapping("/v1/login")
+    public ResponseEntity<LoginResponseDTO> login(RequestEntity<Void> request) {
+        return this.basicAuthenticationHeaderHandler.handle(new RequestDTO(request));
+    }
+}
---
+++ src/main/java/com/crmservice/crmservice/infrastructure/drivers/requesthandlers/BasicAuthorizationHeaderHandler.java
@@ -0,0 +1,69 @@
+package com.crmservice.crmservice.infrastructure.drivers.requesthandlers;
+
+import com.crmservice.crmservice.infrastructure.drivers.logs.LogMessageBuilder;
+import com.crmservice.crmservice.infrastructure.drivers.requests.dtos.RequestDTO;
+import com.crmservice.crmservice.infrastructure.drivers.responses.HttpAdapterResponseBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Base64;
+import java.util.List;
+
+public class BasicAuthorizationHeaderHandler extends BaseRequestHandler<Void, Void> {
+
+    private final Logger logger = LoggerFactory.getLogger(BasicAuthorizationHeaderHandler.class);
+    private static final String BASIC_AUTHORIZATION_TYPE = "Basic";
+
+    @Override
+    public ResponseEntity<Void> handle(RequestDTO<Void> request) {
+
+        ResponseEntity response;
+
+        try {
+            String authorizationToken = parseBasicAuthorizationHeader(getAuthorizationHeader(request));
+            String[] credentials = extractCredentialsFromAuthenticationToken(authorizationToken);
+
+            if (credentials.length != 2 || credentials[0].isBlank() || credentials[1].isBlank()) {
+                String message = "The request username or password is blank.";
+                logger.error(LogMessageBuilder.build(request.getRequestEntity(), message));
+                return HttpAdapterResponseBuilder.malformedExpectedData();
+            }
+
+            request.setContext("requestUsername", credentials[0]);
+            request.setContext("requestPassword", credentials[1]);
+
+            response = super.handle(request);
+
+        } catch (SecurityException ex) {
+            response = HttpAdapterResponseBuilder.malformedExpectedData();
+            logger.error(LogMessageBuilder.build(request.getRequestEntity(), ex.getMessage(), getStackTrace(ex.getStackTrace())));
+        }
+
+        return response;
+    }
+
+    private List<String> getAuthorizationHeader(RequestDTO<Void> request) {
+        return request.getRequestEntity().getHeaders().get("Authorization");
+    }
+
+    private String parseBasicAuthorizationHeader(List<String> authorizationHeader) {
+        String[] parsedAuthorizationHeader = authorizationHeader
+                .stream()
+                .findFirst()
+                .map(value -> value.split("\s"))
+                .orElseThrow(SecurityException::new);
+
+        if (!BASIC_AUTHORIZATION_TYPE.equals(parsedAuthorizationHeader[0])) {
+            throw new SecurityException("Incorrect authorization type.");
+        }
+
+        return parsedAuthorizationHeader[1];
+    }
+
+    private String[] extractCredentialsFromAuthenticationToken(String authenticationToken) {
+        byte[] decodedAuthenticationToken = Base64.getDecoder().decode(authenticationToken);
+        String plainCredentials = new String(decodedAuthenticationToken);
+        return plainCredentials.split(":");
+    }
+}
